74f7f625ace43a0f62b6d971cad629cba36c80eb
PC <liza.cherpak1@gmail.com>
2017-10-10 18:56:55 +0300

    add board

---
+++ board.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<title>Bootstrap Template</title>
+	<!-- Bootstrap -->
+	<link  href="css/bootstrap.css" rel="stylesheet">
+	<link  href="style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="well  span12 "><h1>SmartHome </h1>
+			</div>
+			<div class="well graphic span12">
+				
+			</div>
+
+		</div>
+		<div class="row"> <div class=" span4">
+			<button type="submit" name="submit" class="btn btn-block btn-seccess btn-inverse"> <h5 class="text-warning">Устройство 1</h5></button>
+    </div>
+    <div class=" span4 offset4">
+			<button type="submit" name="submit" class="btn btn-block btn-seccess btn-inverse"> <h5 class="text-warning">Устройство 2</h5></button>
+    </div>
+		</div>
+	</div>
+</body>
+</html>
