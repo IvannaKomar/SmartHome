3f1c3b1c654579daa156b3662ca55f1935037f03
ivanna <kivanna007@gmail.com>
2017-10-10 19:04:43 +0300

    fix

--- index.php
+++ index.php
@@ -1,49 +1,47 @@
 <?php
+require 'conf.php';
 session_start();
 $error = "";
 if (isset($_POST['submit'])) {
-    if ($_POST['login'] == USER and  $_POST['password'] == PASSWORD) {
+    if ($_POST['login'] == USER and $_POST['password'] == PASSWORD) {
         $_SESSION['user'] = $_POST['login'];
-        
+        var_dump("ff");
+        header('Location: board.php');
     } else {
-        $error = "Wrong password";
+        $error = "Введены неверные данные!!!";
     }
 }
-
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>SmartHome</title>
- <link href="css/bootstrap.min.css" rel="stylesheet">
-<style>
-.well{
-    margin-top: 240px
-}
-</style>
-<meta charset="UTF-8">
-        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link  href="style.css" rel="stylesheet">
+        <meta charset="UTF-8">
     </head>
     <body>
         <div class="container">
-<div class ="row">
-<div class ="well span4 offset4">
-<legend>Авторизация</legend>
-<div class="alert alert-error">
-<a class="close" href="#" data-dismiss="alert">x</a>Введены неверные данные!!!
-</div>
+            <div class ="row">
+                <div class ="well span4 offset4">
+                    <legend>Авторизация</legend>
+                    <?php if (!empty($error)) : ?>
+                        <div class="alert alert-error">
+                            <?php echo $error; ?>
+                            <a class="close" href="#" data-dismiss="alert">x</a>
+                        </div>
+                    <?php endif; ?>
 
+                    <form method="POST" action="" accept-charset="UTF-8">
+                        <input type="text" class="span4" placeholder="Ваш логин" name="login">
+                        <input type="password" class="span4" placeholder="Ваш пароль" name="password">
+                        <button type="submit" name="submit" class="btn btn-block btn-success">Авторизоваться</button>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-<form method="POST" action="" accept-charset="UTF-8">
-<input type="text" class="span4" placeholder="Ваш логин" name="login">
-<input type="password" class="span4" placeholder="Ваш пароль" name="password">
-<button type="submit" name="submit" class="btn btn-block btn-success">Авторизоваться</button>
-</form>
-</div>
-</div>
-</div>
-
-<script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
 
