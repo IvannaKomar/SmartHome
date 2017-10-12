264426360559816a5f4bd47681efd765c7dc7057
otis <pro100print@yahoo.com>
2017-10-12 19:46:26 +0300

    git push

--- board.php
+++ board.php
@@ -6,35 +6,31 @@ if (empty($_SESSION['user'])) {
 }
 if (isset($_POST['device1'])) {
     $currentState = $pdo->query('SELECT state FROM devices WHERE number = 1 ORDER BY number')->fetchColumn(0);
-    
-$newState = ($currentState == "OFF") ? "ON" : 'OFF' ;
-$pdo->query("UPDATE devices SET state = '{$newState}' WHERE number = 1");    
-  
+
+    $newState = ($currentState == "OFF") ? "ON" : 'OFF';
+    $pdo->query("UPDATE devices SET state = '{$newState}' WHERE number = 1");
 }
 
 if (isset($_POST['device2'])) {
     $currentState = $pdo->query('SELECT state FROM devices WHERE number = 2 ORDER BY number')->fetchColumn(0);
-      
-$newState = ($currentState == "OFF") ? "ON" : 'OFF' ;
-$pdo->query("UPDATE devices SET state = '{$newState}' WHERE number = 2");    
-  
+
+    $newState = ($currentState == "OFF") ? "ON" : 'OFF';
+    $pdo->query("UPDATE devices SET state = '{$newState}' WHERE number = 2");
 }
 
 $res = $pdo->query('SELECT number, state FROM devices ORDER BY number')->fetchAll();
 $button1 = $res[0];
 $button2 = $res[1];
 
-$button1Class = ($button1["state"] == "OFF") ? 'btn-success' : 'btn-danger' ;
+$button1Class = ($button1["state"] == "OFF") ? 'btn-success' : 'btn-danger';
 
-$button2Class = ($button2["state"] == "OFF") ? 'btn-success' : 'btn-danger' ;
+$button2Class = ($button2["state"] == "OFF") ? 'btn-success' : 'btn-danger';
 
-
-
-
+$temperatures = $pdo->query("SELECT * FROM temperatures ORDER BY id DESC LIMIT 20")->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="ru"
-    <head>
+      <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width-device-width, initial-scale=1">
@@ -42,6 +38,8 @@ $button2Class = ($button2["state"] == "OFF") ? 'btn-success' : 'btn-danger' ;
         <!-- Bootstrap -->
         <link  href="css/bootstrap.css" rel="stylesheet">
         <link  href="style.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>    
+        <script src="https://code.highcharts.com/highcharts.js"></script>        
     </head>
     <body>
         <div class="container">
@@ -49,7 +47,7 @@ $button2Class = ($button2["state"] == "OFF") ? 'btn-success' : 'btn-danger' ;
                 <div class="span12 "><h1>SmartHome </h1>
 
                 </div>
-                <div class="graphic span12">
+                <div id="container" class="graphic span12">
 
                 </div>
 
@@ -62,5 +60,72 @@ $button2Class = ($button2["state"] == "OFF") ? 'btn-success' : 'btn-danger' ;
                     </div> </form>
             </div>
         </div>
+        <script>
+            var temperatures = <?php echo json_encode($temperatures); ?>;
+            $(document).ready(function () {
+                Highcharts.setOptions({
+                    global: {
+                        useUTC: false
+                    }
+                });
+
+                Highcharts.chart('container', {
+                    chart: {
+                        type: 'spline',
+                        animation: Highcharts.svg, // don't animate in old IE
+                        marginRight: 10,
+                    },
+                    title: {
+                        text: 'Temperature'
+                    },
+                    xAxis: {
+                        type: 'datetime',
+                        tickPixelInterval: 150
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Value'
+                        },
+                        plotLines: [{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }]
+                    },
+                    tooltip: {
+                        formatter: function () {
+                            return '<b>' + this.series.name + '</b><br/>' +
+                                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                                    Highcharts.numberFormat(this.y, 2);
+                        }
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    exporting: {
+                        enabled: false
+                    },
+                    series: [{
+                            name: 'Random data',
+                            data: (function () {
+                                // generate an array of random data
+                                var data = [], i;
+
+
+                                for (i = 0; i < temperatures.length; i++) {
+                                    var temp = temperatures[i];
+                                    console.log(temp);
+                                    data.push({
+                                        x: new Date(temp.create_date).getTime(),
+                                        y: temp.value
+                                    });
+                                }
+                                return data;
+                            }())
+                        }]
+                });
+            });
+        </script>
+
     </body>
 </html>
